90ff30faab0413864e7f0679afecae306e5d1a1b
pmiri <pmiriello@gmail.com>
2017-10-14 17:26:15 -0400

    static directories

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static('static'))
+
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
