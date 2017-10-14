bcdd76062cf90083c31511a76bdf99b2c0106db5
pmiri <pmiriello@gmail.com>
2017-10-14 18:23:48 -0400

    default sendback

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ app.get('/', function (req, res) {
 })
 
 app.get('/menu/show', function(req, res){
-    res.send('you got the menu')
+    res.send(JSON.stringify({
+        "speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
+        "displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+        "data": {},
+        "contextOut": [],
+        "source": "DuckDuckGo"
+        }))
 })
 
 
