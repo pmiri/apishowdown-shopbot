3559641405fc5a51b857dc91ded8731fef7705ad
pmiri <pmiriello@gmail.com>
2017-10-14 23:01:50 +0000

    a bunch of stuff

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const http = require('http')
 const https = require('https')
 
+var bodyParser = require('body-parser')
+var session = require('express-session')
+var cookieParser = require('cookie-parser')
+var sessionStore = require('sessionstore')
+
 require('dotenv').config()
 
 const app = express()
@@ -12,11 +17,30 @@ app.use(express.static('static'))
 
 app.use(express.static(__dirname + '/public'))
 
+app.enable('trust proxy');
+app.use(bodyParser());
+app.use(cookieParser());
+app.use(session({
+   secret: 'Super Secret Password',
+   proxy: true,
+   key: 'session.sid',
+   cookie: {secure: true},
+//NEVER use in-memory store for production - I'm using mongoose/mongodb here
+   store: sessionStore.createSessionStore()
+}));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.get('/menu/show', function(req, res){
+app.post('/', function (req, res) {
+ console.log('post made enfin')
+ res.send('Hello World!')
+})
+
+app.post('/menu', function(req, res){
+	console.log('post made')
+    res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({
         "speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
         "displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
