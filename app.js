466857136272a534ddfa6bb87cfc105be493ce94
pmiri <pmiriello@gmail.com>
2017-10-14 20:19:11 -0400

    post returns values

--- app.js
+++ app.js
@@ -35,21 +35,15 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
  console.log('post made enfin')
- res.send('Hello World!')
+ res.setHeader('Content-Type', 'application/json');
+ res.send(JSON.stringify({
+     "speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
+     "displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+     "data": {},
+     "contextOut": [],
+     "source": "DuckDuckGo"
+     }))
 })
-
-app.post('/menu', function(req, res){
-	console.log('post made')
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({
-        "speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
-        "displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-        "data": {},
-        "contextOut": [],
-        "source": "DuckDuckGo"
-        }))
-})
-
 
 http.createServer(app).listen(3000, function(){
     console.info('http listening on port: ' + 3000)
